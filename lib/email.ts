e4955f511fbc56c711c3ab573a7d3c416bbfb006
imbabaki <nerrenze.marats@ustp.edu.ph>
2026-02-27 09:44:47 +0800

    Fixed Bug

--- lib/email.ts
+++ lib/email.ts
@@ -1,8 +1,7 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function sendOtpEmail(to: string, otp: string) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const { data, error } = await resend.emails.send({
       from: process.env.RESEND_FROM_EMAIL ?? 'DOST X - Misamis Oriental <onboarding@resend.dev>',
